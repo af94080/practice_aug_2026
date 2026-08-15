064142ac9318241ff7101f72f87c241244e13b9d
arul <arulfranciswork@gmail.com>
2026-08-14 23:51:59 -0700

    Create deepseek_perf_testing_aug_14.py
    try to find how much of the token usage is spent on reasoning for deepseek

---
+++ deepseek_perf_testing_aug_14.py
@@ -0,0 +1,55 @@
+import os
+import time 
+from dotenv import load_dotenv
+load_dotenv()
+
+MODEL = "deepseek-ai/DeepSeek-V4-Flash-0731"
+
+HF_API_KEY = os.environ["HF_API_KEY"]
+
+from huggingface_hub import InferenceClient 
+
+client = InferenceClient(
+	provider="together",
+	api_key=HF_API_KEY
+	)
+
+def get_response(user_prompt):
+	start_time = time.perf_counter()
+	response = client.chat.completions.create(
+	model=MODEL,
+	messages=[
+        {
+		"role": "user",
+		"content": user_prompt
+        }
+    ],
+	max_tokens=1000
+	)
+	run_time = time.perf_counter() - start_time
+
+	return response, run_time  
+
+def main():
+
+	questions = [
+	    "What is the capital of France?",
+	    "Explain why the sky appears blue in two sentences.",
+	    "Give me three advantages of using Snowflake.",
+	    "Write a Python function that reverses a string.",
+	    "What is the difference between an API and an SDK?"
+	]
+
+	for question in questions:
+
+		user_prompt = question
+		response, runtime = get_response(user_prompt)
+		print(f"ANSWER: {response.choices[0].message.content}")
+		print(f"Runtime: {runtime:.2f} seconds")
+		print(f"Prompt tokens: {response.usage.prompt_tokens}")
+		print(f"Completion tokens: {response.usage.completion_tokens}")
+
+		print(f"Reasoning tokens: {response.usage.completion_tokens_details['reasoning_tokens']}")
+		print(f"Total tokens: {response.usage.total_tokens}") 
+
+main()
